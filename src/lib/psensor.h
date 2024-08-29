53fd3b1a5fb5cf92920df248d87582a453e59688
0xSYS <92180085+0xSYS@users.noreply.github.com>
2024-08-29 23:43:09 +0300

    I again forgor something...

--- src/lib/psensor.h
+++ src/lib/psensor.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2016 jeanfi@gmail.com
+ * Copyright (C) 2010-2024 jeanfi@gmail.com, 0xSYS
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
