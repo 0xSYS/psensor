1142fe10074005a75e3f7ec9d723548a14522944
0xSYS <92180085+0xSYS@users.noreply.github.com>
2024-09-21 00:01:44 +0300

    Delete scripts/smatch.sh

--- scripts/smatch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-SMATCH_DIR=$HOME/soft/smatch
-
-SCRIPT_DIR=`dirname $0`
-
-cd $SCRIPT_DIR/..
-make clean
-make CHECK="$SMATCH_DIR/smatch --full-path" CC=$SMATCH_DIR/cgcc | tee warns.txt
