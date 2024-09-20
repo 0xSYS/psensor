e55a0afc08d00379bdad7ca2dc7eabe3a8dab3b5
0xSYS <92180085+0xSYS@users.noreply.github.com>
2024-09-20 23:59:28 +0300

    Delete tests directory

--- tests/test-cppcheck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-EIDS=performance,portability
-ROOTDIR="$srcdir/.."
-INCLUDE_OPTS="-q -f -I$ROOTDIR/src/lib -I$ROOTDIR/src -I$ROOTDIR -I$ROOTDIR/.."
-
-cppcheck $ROOTDIR/src $INCLUDE_OPTS --enable=$EIDS --quiet --error-exitcode=1 || exit 1
