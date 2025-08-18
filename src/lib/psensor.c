0d59bca76ef96d89c07c7083a7944cba3bc07557
0xSYS <andreicu07@gmail.com>
2025-08-18 10:22:12 +0300

    fixed code formatting mistake + added license to new ui source files

--- src/lib/psensor.c
+++ src/lib/psensor.c
@@ -293,7 +293,7 @@ struct measure *psensor_get_current_measure(struct psensor *sensor)
  * Returns the minimal value of a given 'type' (SENSOR_TYPE_TEMP or
  * SENSOR_TYPE_FAN)
  */
-static double get_min_value(struct psensor **sensors, int type)\
+static double get_min_value(struct psensor **sensors, int type)
 {
     double m = UNKNOWN_DBL_VALUE;
     struct psensor **s = sensors;
@@ -320,10 +320,8 @@ static double get_min_value(struct psensor **sensors, int type)\
         }
         s++;
     }
-    s++;
-  }
-
-  return m;
+  
+    return m;
 }
 
 /*
