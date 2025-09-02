56fb7ccf5435b884a42d642911d307033d8b3de7
0xSYS <andreicu07@gmail.com>
2025-09-02 14:08:52 +0300

    fixing module loader...

--- src/lib/pmod.c
+++ src/lib/pmod.c
@@ -50,17 +50,17 @@ bool is_mod_existing(const char * mod_name)
     
     if(ret < 0)
     {
-        log_error("Lookup failed for module: %s", mod_name);
+        log_error("CHECK_MOD_EXIST: Lookup failed for module: %s", mod_name);
         return false;
     }
     else if(!list)
     {
-        log_error("Module %s not found in module directories", mod_name);
+        log_error("CHECK_MOD_EXIST: Module %s not found in module directories", mod_name);
         return false;
     }
     else
     {
-        log_info("Module %s exists on disk", mod_name);
+        log_info("CHECK_MOD_EXIST: Module %s exists on disk", mod_name);
         kmod_module_unref_list(list);
     }
     return true;
@@ -83,51 +83,52 @@ void pmod_load_modules()
     */
     
     for(int i = 0; module_table[i].name != NULL; i++)
+    {
+        if(create_mod_obj(module_table[i].name, kmod_ctx, &modules[i]) == 0)
         {
-            if(create_mod_obj(module_table[i].name, kmod_ctx, &modules[i]) == 0)
+            int ret;
+            
+            struct kmod_list *deps = kmod_module_get_dependencies(modules[i]);
+            struct kmod_list *itr;
+            
+            kmod_list_foreach(itr, deps)
             {
-                int ret;
+                struct kmod_module *dep = kmod_module_get_module(itr);
+                const char *dep_name = kmod_module_get_name(dep);
+            
+                log_info("Dependency: %s", dep_name);
+                int ret = kmod_module_insert_module(dep, 0, NULL);
+                if(ret < 0)
+                    log_error("Failed to insert dependency %s (%s)", dep_name, strerror(-ret));
                 
-                struct kmod_list *deps = kmod_module_get_dependencies(modules[i]);
-                struct kmod_list *itr;
-                
-                kmod_list_foreach(itr, deps) {
-                    struct kmod_module *dep = kmod_module_get_module(itr);
-                    const char *dep_name = kmod_module_get_name(dep);
-                
-                    log_info("Dependency: %s", dep_name);
-                    int ret = kmod_module_insert_module(dep, 0, NULL);
-                    if (ret < 0) {
-                        log_error("Failed to insert dependency %s (%s)", dep_name, strerror(-ret));
-                    }
-                
-                    kmod_module_unref(dep);
-                }
-                
-                kmod_module_unref_list(deps);
-                
-                // Load the module only if it's not already loaded
-                if(is_mod_existing(module_table[i].name) && !pmod_check_loaded(module_table[i].name))
-                {
-                    ret = kmod_module_insert_module(modules[i], 0, module_table[i].opts);
-                    
-                    if(ret == 0)
-                    {
-                        log_info("Loaded module: %s", module_table[i].name);
-                    }
-                    else
-                    {
-                        log_error("Failed to load module %s via libkmod (%s)", module_table[i].name, strerror(-ret));
-                        //if (fallback_modprobe(module_table[i].name, module_table[i].opts) != 0)
-                        //    log_error("Failed to load module %s via modprobe", module_table[i].name);
-                    }
-                }
+            
+                kmod_module_unref(dep);
             }
-            else
+            
+            kmod_module_unref_list(deps);
+            
+            // Load the module only if it's not already loaded
+            if(is_mod_existing(module_table[i].name) && !pmod_check_loaded(module_table[i].name))
             {
-                log_error("Cannot create module object for %s, skipping", module_table[i].name);
+                ret = kmod_module_insert_module(modules[i], 0, module_table[i].opts);
+                
+                if(ret == 0)
+                {
+                    log_info("MOD_LOADER: Loaded module: %s", module_table[i].name);
+                }
+                else
+                {
+                    log_error("MOD_LOADER: Failed to load module %s via libkmod (%s)", module_table[i].name, strerror(-ret));
+                    //if (fallback_modprobe(module_table[i].name, module_table[i].opts) != 0)
+                    //    log_error("Failed to load module %s via modprobe", module_table[i].name);
+                }
             }
         }
+        else
+        {
+            log_error("MOD_LOADER: Cannot create module object for %s, skipping", module_table[i].name);
+        }
+    }
     
     // Free stuff
     for(int i = 0; module_table[i].name != NULL; i++)
@@ -158,19 +159,19 @@ bool pmod_check_loaded(const char * mod_name)
     state = kmod_module_get_initstate(m);
     if(state == KMOD_MODULE_LIVE)
     {
-        log_info("Module %s is loaded", mod_name);
+        log_info("CHECK_MOD_LOADED: Module %s is loaded", mod_name);
         kmod_module_unref(m);
         return true;
     }
     else if(state == KMOD_MODULE_BUILTIN)
     {
-        log_info("Module %s is built into the kernel", mod_name);
+        log_info("CHECK_MOD_LOADED: Module %s is built into the kernel", mod_name);
         kmod_module_unref(m);
         return true;
     }
     else
     {
-        log_info("Module %s is not loaded (state: %d)", mod_name, state);
+        log_info("CHECK_MOD_LOADED: Module %s is not loaded (state: %d)", mod_name, state);
         kmod_module_unref(m);
         return false;
     }
