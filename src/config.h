fe4c48f9db8cbedd026a76b1fd28b40ca81db7ac
0xSYS <andreicu07@gmail.com>
2025-09-11 00:10:12 +0300

    version change...

--- src/config.h
+++ src/config.h
@@ -140,7 +140,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.2.0"
+#define VERSION "1.2.1 Beta"
 
 /* Number of bits in a file offset, on hosts where this is settable. */
 /* #undef _FILE_OFFSET_BITS */
