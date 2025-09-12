d4b24fe14e497b55539a1407f9f028f8de3cbcb2
0xSYS <andreicu07@gmail.com>
2025-09-13 00:24:29 +0300

    le comment

--- src/new-gui/UI/ui.cpp
+++ src/new-gui/UI/ui.cpp
@@ -263,7 +263,7 @@ void RenderPreferences()
                 SaveSettings();
             }
             
-            if(ImGui::Checkbox("Autosave settings", &autosave_settings))
+            if(ImGui::Checkbox("Autosave settings", &autosave_settings)) // When disabled it already prevents this setting from saving to json file :madman:
             {
                 liveSettings.autosave_settings = autosave_settings;
                 SaveSettings();
